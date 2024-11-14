6dba5f64237e5c0a4d745099598ee3130b94b969
Takumi Kobayashi <takumi.kobayashi@quest.co.jp>
2024-11-14 16:24:09 +0900

    Initial commit

--- marketplace/amplify/functions/Api/api/PaymentController.ts
+++ marketplace/amplify/functions/Api/api/PaymentController.ts
@@ -16,15 +16,106 @@ export class PaymentController  extends RequestValidator {
   private static paymentController: PaymentController | null = null;
 
   public static createPaymentUrl = async (req: Request, res: Response): Promise<void> => {
-    console.error(`The payment sdk is not yet setting.`);
-    res.status(500).json({result:false, message:'Internal Server Error (The payment sdk is not yet setting)'}).end();
-    return;
+    const controller = await PaymentController.getController();
+    const validateRequest = await controller.validatePostRequest(req);
+    if (!validateRequest) {
+      res.status(401).json({ result: false, message: 'Unauthorized' }).end();
+      return;
+    }
+
+    const userId: string = controller.userId;
+    const productId = req.body.productId;
+
+    if (typeof productId !== "string") {
+      res.status(403).json({ result: false, message: 'Forbidden' }).end();
+      return;
+    }
+
+    // 商品情報の取得
+    const product = await getProduct(productId);
+    if (!product) {
+      res.status(404).json({ result: false, message: 'Not Found' }).end();
+      return;
+    }
+
+    const productInventories = await getProductInventories(productId);
+    // active な在庫を一つ取得
+    const inventory = productInventories.find((item) => item.inventoryStatus === "active");
+    if (!inventory) {
+      res.status(404).json({ result: false, message: 'Not Found' }).end();
+      return;
+    }
+
+    const totalAmount = product.productPrice;
+    const totalTax = product.productTax;
+    const host = req.headers.host || "";
+    const orderId = `JDP${controller.getRandomString(24)}`;
+
+    const gmoExpireDate = dayjs().tz("Asia/Tokyo").add(5, "minute");
+    const param: LinkPlusUrlPaymentParam = {
+      orderId: orderId,
+      amount: totalAmount,
+      tax: totalTax,
+      redirectUrl: `https://${host}/prod/api/payment/callback/?orderId=${orderId}&redirect_host=${req.headers["origin"]}`,
+      clientField1: userId,
+      clientField2: `商品 ${product.productName}`,
+      clientField3: `トークンID ${inventory.resourceId}`,
+      paymentExpireDate: gmoExpireDate.format("YYYYMMDDHHmm"),
+    };
+
+    try {
+      const result = await getLinkPlusUrlPayment(param);
+      const order = await createProductOrder({
+        userId, orderId, amount: totalAmount, tax: totalTax, linkUrl: result.LinkUrl, linkUrlExpiredAt: gmoExpireDate.unix(), productId: product.productId, productType: product.productType, accessId: result.AccessID, accessPass: result.AccessPass,
+      });
+      if (!order) {
+        res.status(500).json({ result: false, message: 'Internal Server Error' }).end();
+        return;
+      }
+      // gmoExpireDate + 5分後に在庫を予約.
+      await reserveProductInventory(productId, inventory.resourceId, orderId, gmoExpireDate.add(5, "minute").unix());
+      res.status(200).send({
+        result: true,
+        url: result.LinkUrl,
+        orderId,
+      }).end();
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ result: false, message: 'Internal Server Error' }).end();
+    }
   }
 
   public static callbackApiPayment = async (req: Request, res: Response): Promise<void> => {
-    console.error(`The payment sdk is not yet setting.`);
-    res.status(403).end();
-    return;
+    if (typeof req.query.orderId !== "string" || typeof req.query.redirect_host !== "string") {
+      res.status(403).end();
+      return;
+    }
+
+    const order = await getProductOrder(req.query.orderId);
+    if (!order) {
+      console.error(`Order not found: ${req.query.orderId}`);
+      res.status(403).end();
+      return;
+    }
+
+    if (order?.orderStatus === "UNPROCESSED") {
+      // wait を入れる.
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+
+    // req.body の Buffer をパース
+    const params = new URLSearchParams(req.body.toString());
+    const result = params.get("result");
+    if (result) {
+      const gmoResult = parseGmoResult(result);
+      if (gmoResult.transactionresult.Result === "PAYSUCCESS") {
+        const accessId = gmoResult.transactionresult.AccessID;
+        const accessPass = gmoResult.transactionresult.AccessPass;
+        await updateProductOrderAccessInfo(req.query.orderId, accessId, accessPass);
+        return res.redirect(`${req.query.redirect_host}/products/complete/?orderId=${req.query.orderId}`);
+      }
+    }
+    res.redirect(`${req.query.redirect_host}/products/${order?.productId}/?orderId=${req.query.orderId}`);
   }
 
   private getRandomString = (length: number): string => {
--- marketplace/amplify/functions/Api/util/payment.ts
+++ marketplace/amplify/functions/Api/util/payment.ts
@@ -56,10 +56,84 @@ const gmoShopId = process.env.GMO_SHOP_ID || "";
 const gmoShopPass = process.env.GMO_SHOP_PASS || "";
 const gmoLinkPlusConfigId = process.env.GMO_LINK_PLUS_CONFIG_ID || "";
 
-export const getLinkPlusUrlPayment = async (param: LinkPlusUrlPaymentParam): Promise<void> => {}
+export const getLinkPlusUrlPayment = async (param: LinkPlusUrlPaymentParam): Promise<LinkPlusUrlPaymentResponse> => {
+  const requestParam = {
+    configid: gmoLinkPlusConfigId,
+    transaction: {
+      OrderID: param.orderId,
+      Amount: param.amount,
+      Tax: param.tax,
+      ClientField1: param.clientField1 ?? " ",
+      ClientField2: param.clientField2 ?? " ",
+      ClientField3: param.clientField3 ?? " ",
+      PayMethods: ["credit"],
+      RetUrl: param.redirectUrl,
+      PaymentExpireDate: param.paymentExpireDate ?? "",
+      Detail: param.clientField2 ?? "",
+    },
+    geturlparam: {
+      ShopID: gmoShopId,
+      ShopPass: gmoShopPass,
+    },
+    displaysetting: {
+      ShopName: "JDPマーケットプレイスサンプル",
+    },
+    credit: {
+      JobCd: "CAPTURE",
+      Method: "1",
+    },
+  };
+  try {
+    const res = await axios.post<LinkPlusUrlPaymentResponse>(`${gmoHost}/payment/GetLinkplusUrlPayment.json`, requestParam, {
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+    });
+    return res.data;
+  } catch (e) {
+    console.error(e);
+    if (isAxiosError(e)) {
+      console.error(e.response?.data);
+    }
+    throw new Error("Payment error.");
+  }
+};
 
 // GMO からの result を解析する
-export const parseGmoResult = "";
+export const parseGmoResult = (result: string): ParsedGmoResultResponse => {
+  // result の解析. ドット で分割.
+  const resultArray = result.split(".");
+  // 最初の文字列と gmoShopPass を連結してsha256 でハッシュ化
+  const hash = crypto.createHash('sha256').update(resultArray[0] + gmoShopPass).digest('hex');
 
-export const rejectLinkPlusUrlPayment = async (orderId: string): Promise<void> => {}
+  // ハッシュと result の最後の文字列が一致するかどうか
+  if (hash === resultArray[resultArray.length - 1]) {
+    // 一致していれば result をデコードして返す
+    const resultString = Buffer.from(resultArray[0], 'base64').toString();
+    return JSON.parse(resultString) as ParsedGmoResultResponse;
+  } else {
+    throw new Error("Invalid result.");
+  }
+}
 
+export const rejectLinkPlusUrlPayment = async (orderId: string): Promise<RejectLinkPlusUrlPaymentResponse> => {
+  const requestParam = {
+    ShopID: gmoShopId,
+    ShopPass: gmoShopPass,
+    OrderID: orderId,
+  };
+  try {
+    const res = await axios.post<RejectLinkPlusUrlPaymentResponse>(`${gmoHost}/payment/RejectLinkplusUrlPayment.json`, requestParam, {
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+    });
+    return res.data;
+  } catch (e) {
+    console.error(e);
+    if (isAxiosError(e)) {
+      console.error(e.response?.data);
+    }
+    throw new Error("Payment error.");
+  }
+};
